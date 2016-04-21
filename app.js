ddbcb319b3953914a7e8231a7f6c444fcdb3c1d7
Stephane <stephane@sbnet.fr>
2016-04-21 13:00:02 +0200

    avec socket.io

--- app.js
+++ app.js
@@ -1,49 +1,44 @@
 var app = require('express')(),
-    session = require('cookie-session'), // Charge le middleware de sessions
-    bodyParser = require('body-parser'), // Charge le middleware de gestion des paramètres
-    urlencodedParser = bodyParser.urlencoded({ extended: false }),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
-    ent = require('ent'), // Permet de bloquer les caractères HTML 
+    todoliste = [], // stocke la todolist
+    bodyParser = require('body-parser'),
+    ent = require('ent'), // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
     fs = require('fs');
 
-/* On utilise les sessions */
-app.use(session({secret: 'todotopsecret'}))
-
-
-/* S'il n'y a pas de todolist dans la session,
-on en crée une vide sous forme d'array avant la suite */
-.use(function(req, res, next){
-    if (typeof(req.session.todolist) == 'undefined') {
-        req.session.todolist = [];
-    }
-    next();
-})
+/* permet d'utiliser body dans la request */
+app.use(bodyParser());
 
 /* On affiche la todolist et le formulaire */
-.get('/todo', function(req, res) { 
-    res.render('todo.ejs', {todolist: req.session.todolist});
+app.get('/todo', function(req, res) {     
+    res.render('todo.ejs', todoliste);
 })
-
-/* On ajoute un élément à la todolist */
-.post('/todo/ajouter/', urlencodedParser, function(req, res) {
+ 
+/* On ajoute un élément à la todolist et le stoche dans newtodo*/
+.post('/todo/ajouter/', function(req, res) {
     if (req.body.newtodo != '') {
-        req.session.todolist.push(req.body.newtodo);
+        todoliste.push(req.body.newtodo);
     }
     res.redirect('/todo');
 })
-
+ 
 /* Supprime un élément de la todolist */
 .get('/todo/supprimer/:id', function(req, res) {
     if (req.params.id != '') {
-        req.session.todolist.splice(req.params.id, 1);
+        todoliste.splice(req.params.id, 1);
     }
     res.redirect('/todo');
 })
-
+ 
 /* On redirige vers la todolist si la page demandée n'est pas trouvée */
-.use(function(req, res, next){
+.use(function(req, res, next) {
     res.redirect('/todo');
-})
+});
+ 
+server.listen(8080);
 
-.listen(8080);
+/* on lance l'écoute sur la connexion d'un nouvel utilisateur */
+io.sockets.on('connection', function (socket, pseudo) {
+    // On met a jour tous les clients
+    io.sockets.emit("miseajour", todoliste);
+});
